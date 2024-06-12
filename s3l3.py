1b96610ca92c881e782354233e1d682541fb7f2a
Sarahsortiz <sarahortiz2001@gmail.com>
2024-06-12 11:34:01 -0400

    Add files via upload

---
+++ s3l3.py
@@ -0,0 +1,24 @@
+def perimetro():
+    print ("Calcolare il perimetro della figura geometrica scelta")
+    print ("Quadrato: 1")
+    print ("Rettangolo: 2")
+    print ("Cerchio: 3")
+  
+  print ("Inserire la scelta:")
+  scelta = int(input(""))
+  if scelta = 1:
+      print ("Hai scelto: Quadrato")
+      lato = float (input("Inserire dato lato del Quadrato"))
+      print ("Il perimetro calcolato è, avente il lato,lato, lato*4")
+    elif scelta = 2:
+        print ("Hai scelto: Rettangolo")
+        base = float(input("Inserire dato base del Rettangolo"))
+        altezza = float(input("Inserire dato altezza del Rettangolo"))
+        print ("Il perimetro calcolato è, avente base, base e altezza, altezza, base*altezza del Rettangolo")
+
+    elif scelta = 3:
+        print ("Hai scelto: Cerchio")
+        raggio = float(input("Inserire dato raggio del Cerchio"))
+        print ("Il perimetro calcolaro è, avente raggio, r, 2*r*3,14")
+else:
+    print ("Inserire la scelta")
